76e88f08ab7b61ad833746febe3abd01e952693a
Max <maxmanski@live.at>
2015-03-23 12:08:32 +0100

    test

---
+++ Neues Textdokument.bat
@@ -0,0 +1,11 @@
+echo # web6-1 >> README.md
+
+git init
+
+git add README.md
+
+git commit -m "first commit"
+
+git remote add origin https://github.com/Maxmanski/web6-1.git
+
+git push -u origin master
