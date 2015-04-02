c50349f6433a3eb821d0b5e6f681f25e4db76e78
Nicole Haberberger <nicole.haberberger@gmail.com>
2015-04-02 23:00:35 +0200

    js validation

---
+++ js/formvalidation.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+	$("#register").submit(function(event){
+	event.preventDefault();
+
+		if(!hasFormValidation()){
+			// must validate everything via JavaScript
+			var username = $("#username").val();
+			var password = $("#password").val();
+			if(username=="" || username.length <4 || username.length >8)
+				$("#username").addClass("wrong");
+			else
+				$("#username").removeClass("wrong");
+			if(password==""|| password.length <4 || password.length >8)
+				$("#password").addClass("wrong");
+			else
+				$("#password").removeClass("wrong");
+		}else{
+
+			if(!hasNativeDateInput()){
+				// must validate 
+				var inDate = $("#birthday").val();
+				if(inDate != "" && inDate.match(/^\d{1,2}.\d{1,2}.(\d{2}|\d{4})$/i) == null){
+					// did not match
+					$("#birthday").addClass("wrong");
+					return false;
+				}
+			}
+		}
+		
+		/*var name = "You";
+		name = $("#firstName").val();
+		alert(name);*/
+		return true;
+	});
+});
